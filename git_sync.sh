6d17bf09d9cbb709ca55ff431c74f368a23fcc41
Ajay Singh <info2azooba@gmail.com>
2023-11-03 13:14:21 +0545

    config files on 03-11-2023 13:14:21

--- git_sync.sh
+++ git_sync.sh
@@ -1,4 +1,6 @@
 cd /config
+git config --global user.name "Ajay Singh"
+git config --global user.email "info2azooba@gmail.com"
 git pull
 git add .
 git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
